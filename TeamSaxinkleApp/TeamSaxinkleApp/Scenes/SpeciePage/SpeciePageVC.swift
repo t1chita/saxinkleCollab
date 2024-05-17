fbb3a03135c6a318613aded51834e9923d3c9655
t1chita <t1chitashvili@gmail.com>
2024-05-17 20:22:26 +0400

    make api calls

--- TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SpeciePage/SpeciePageVC.swift
+++ TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SpeciePage/SpeciePageVC.swift
@@ -9,6 +9,9 @@ import UIKit
 
 
 class SpeciePageVC: UIViewController {
+    //MARK: UIComponents-
+    let searchController = UISearchController(searchResultsController: nil)
+    //MARK: Properties-
     let speciePageView = SpeciePageView()
     let speciePageViewModel = SpeciePageViewModel()
     
@@ -18,5 +21,32 @@ class SpeciePageVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSearchController()
+        getData()
+    }
+    
+    
+    //MARK: Get Data From ViewModel
+    
+    private func getData() {
+        speciePageViewModel.didLoad()
+    }
+    //MARK: setup Navigation SearchController
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search City"
+        
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+}
+
+
+extension SpeciePageVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
     }
 }
--- TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SpeciePage/SpeciePageView.swift
+++ TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SpeciePage/SpeciePageView.swift
@@ -8,5 +8,44 @@
 import UIKit
 
 class SpeciePageView: UIView {
+    //MARK: - UIComponents
+    let headerLabel: UILabel = {
+        let lbl = UILabel ()
+        lbl.text = "Popular Species In Searched Area"
+        lbl.textColor = .label
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
     
+    //MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup UI Components
+    private func setUp() {
+        backgroundColor = .systemBackground
+        setHeaderLabel()
+    }
+    
+    private func setHeaderLabel() {
+        addSubview(headerLabel)
+        setConstraintsToHeaderLabel()
+    }
+    
+    //MARK: - Set Constrainst To UI Components
+    private func setConstraintsToHeaderLabel() {
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: topAnchor, constant: 150),
+            headerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            headerLabel.heightAnchor.constraint(equalToConstant: 30),
+            headerLabel.widthAnchor.constraint(equalToConstant: 300),
+        ])
+    }
 }
