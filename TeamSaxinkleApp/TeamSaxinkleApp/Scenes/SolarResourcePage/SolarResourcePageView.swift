dd56011819fb32cb1e5a87a6e4623e9b7a1882b8
nuca01 <nucaberiashvili16@gmail.com>
2024-05-18 00:57:36 +0400

    create array of labels

--- TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SolarResourcePage/SolarResourcePageView.swift
+++ TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SolarResourcePage/SolarResourcePageView.swift
@@ -14,13 +14,18 @@ class SolarResourcePageView: UIView {
     //MARK: - Properties
     private var delegate: SolarResourcePageViewDelegate
     
-    private var averageDNI: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .left
-        label.textColor = UIColor.label
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    private var labelArray: [UILabel] = {
+        var array = [UILabel]()
+        for _ in stride(from: 0, to: 3, by: 1) {
+            let label = UILabel()
+            label.textAlignment = .left
+            label.numberOfLines = 0
+            label.textColor = UIColor.label
+            label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            array.append(label)
+        }
+        return array
     }()
     
     private var wholeStackView: UIStackView = {
@@ -36,11 +41,13 @@ class SolarResourcePageView: UIView {
     
     private var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
-        searchBar.placeholder = "Search"
+        searchBar.placeholder = "Search address, get solar resource information"
+        searchBar.searchTextField.font = .systemFont(ofSize: 12)
         searchBar.isUserInteractionEnabled = true
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         //ხაზები რომ არ გამოჩნდეს
         searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        searchBar.layer.cornerRadius = 10
         return searchBar
     }()
     
@@ -60,10 +67,10 @@ class SolarResourcePageView: UIView {
     
     //MARK: - Setup UI Components
     private func setupUI() {
-        backgroundColor = .white
+        backgroundColor = .systemBackground
         setWholeStackView()
         setSearchBar()
-        setAverageDNI()
+        setLabels()
     }
     
     private func setWholeStackView() {
@@ -77,9 +84,60 @@ class SolarResourcePageView: UIView {
         setConstraintsToSearchBar()
     }
     
-    private func setAverageDNI() {
-        wholeStackView.addArrangedSubview(averageDNI)
+    private func setLabels() {
+        for label in labelArray {
+            wholeStackView.addArrangedSubview(label)
+        }
     }
+    
+    //MARK: - Helper Methods
+    private func generateAttributedString(with title: String, and data: TimePeriod) -> NSAttributedString {
+        let title = NSAttributedString(string: "Average \(title):\n", attributes: [
+            .font: UIFont.systemFont(ofSize: 24, weight: .bold),
+        ])
+        
+        let subtitleAnnual = NSAttributedString(string: "Annual: ", attributes: [
+            .font: UIFont.systemFont(ofSize: 20, weight: .semibold),
+        ])
+        
+        let textAnnual = NSAttributedString(string: "\(data.annual) W/m²\n", attributes: [
+            .font: UIFont.systemFont(ofSize: 16),
+        ])
+        
+        let subtitleMonthly = NSAttributedString(string: "Monthly:\n", attributes: [
+            .font: UIFont.systemFont(ofSize: 20, weight: .semibold),
+        ])
+        let textMonthly = NSAttributedString(string: generateMonthlyText(with: data), attributes: [
+            .font: UIFont.systemFont(ofSize: 16),
+        ])
+        
+        let combinedString = NSMutableAttributedString()
+        combinedString.append(title)
+        combinedString.append(subtitleAnnual)
+        combinedString.append(textAnnual)
+        combinedString.append(subtitleMonthly)
+        combinedString.append(textMonthly)
+        
+        return combinedString
+    }
+    
+    private func generateMonthlyText(with data: TimePeriod) -> String {
+"""
+January: \(data.monthly.jan) W/m²
+February: \(data.monthly.feb) W/m²
+March: \(data.monthly.mar) W/m²
+April: \(data.monthly.apr) W/m²
+May: \(data.monthly.may) W/m²
+June: \(data.monthly.jun) W/m²
+July: \(data.monthly.jul) W/m²
+August: \(data.monthly.aug) W/m²
+September: \(data.monthly.sep) W/m²
+October: \(data.monthly.oct) W/m²
+November: \(data.monthly.nov) W/m²
+December: \(data.monthly.dec) W/m²
+"""
+    }
+    
     //MARK: - Set Constraints To UI Components
     private func setConstraintsToWholeStackView() {
         NSLayoutConstraint.activate([
@@ -92,7 +150,7 @@ class SolarResourcePageView: UIView {
     
     private func setConstraintsToSearchBar() {
         NSLayoutConstraint.activate([
-            searchBar.heightAnchor.constraint(equalToConstant: 20),
+            searchBar.heightAnchor.constraint(equalToConstant: 40),
         ])
     }
 }
@@ -100,12 +158,13 @@ class SolarResourcePageView: UIView {
 extension SolarResourcePageView: SolarResourcePageVCViewDelegate {
     
     func dataFetched(data: SolarData) {
-        averageDNI.text = String(data.avg_dni.annual)
+        labelArray[0].attributedText = generateAttributedString(with: "DNI", and: data.avg_dni)
         layoutIfNeeded()
     }
     
     func dataDidNotFetch() {
-        averageDNI.removeFromSuperview()
+//        averageDNI.removeFromSuperview()
+//        wholeStackView.layoutIfNeeded()
     }
 }
 
@@ -117,6 +176,7 @@ extension SolarResourcePageView: UISearchBarDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.endEditing(true)
         super.touchesBegan(touches, with: event)
+        searchBar.resignFirstResponder()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
