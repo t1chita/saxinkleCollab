37b77ef2955abddfd391aeec735b2ce059b2e5bf
nuca01 <nucaberiashvili16@gmail.com>
2024-05-18 16:59:21 +0400

    create custom reusable searchbar

---
+++ TeamSaxinkleApp/TeamSaxinkleApp/Scenes/SolarResourcePage/CustomSearchBar.swift
@@ -0,0 +1,36 @@
+//
+//  CustomSearchBar.swift
+//  TeamSaxinkleApp
+//
+//  Created by nuca on 18.05.24.
+//
+
+import UIKit
+
+class CustomSearchBar: UISearchBar {
+
+    //MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSearchBar()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSearchBar()
+    }
+
+    //MARK: - Setup UI
+    private func setupSearchBar() {
+        searchTextField.font = .systemFont(ofSize: 12)
+        isUserInteractionEnabled = true
+        translatesAutoresizingMaskIntoConstraints = false
+        //ხაზები რომ არ გამოჩნდეს
+        setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        layer.cornerRadius = 10
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.layer.borderColor = UIColor.clear.cgColor
+        searchTextField.layer.borderWidth = 1
+        searchTextField.layer.cornerRadius = 10
+    }
+}
